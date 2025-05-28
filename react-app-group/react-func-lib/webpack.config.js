d4ce06f15620149d42a7b6214031b18f5c2d53e8
Frank Xu <386277405@qq.com>
2025-05-28 22:21:41 +0800

    add ui denpendy

---
+++ react-app-group/react-func-lib/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'react-func-lib.js',
+    library: 'MyReactLib',
+    libraryTarget: 'umd',
+    globalObject: 'this'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  externals: {
+    react: 'commonjs react',
+    'react-dom': 'commonjs react-dom'
+  }
+};
