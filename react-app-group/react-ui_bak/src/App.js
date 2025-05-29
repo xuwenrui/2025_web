ae18e6c72dd749b63fbcc7c006083c382337f4e9
Frank Xu <386277405@qq.com>
2025-05-29 21:56:53 +0800

    change name

---
+++ react-app-group/react-ui_bak/src/App.js
@@ -0,0 +1,33 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { Button } from "react-dom-lib";
+import { add } from "react-func-lib";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <Button
+          label="Click Me"
+          onClick={() => {
+            alert("Clicked!" + add(1, 2));
+          }}
+        />
+      </header>
+    </div>
+  );
+}
+
+export default App;
